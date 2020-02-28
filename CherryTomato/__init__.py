b346a1c7aee57717661645d3b47d0b86d7de3dcc
yakimka <ss.yakim@gmail.com>
2020-02-28 15:59:39 +0200

    Update version

--- CherryTomato/__init__.py
+++ CherryTomato/__init__.py
@@ -4,6 +4,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 APP_ICON = os.path.join(MEDIA_DIR, 'icon.png')
 
-VERSION = '0.4.2'
+VERSION = '0.5.0'
 ORGANIZATION_NAME = 'yakimka'
 APPLICATION_NAME = 'CherryTomato'
