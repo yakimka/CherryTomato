5654ce1356f18291561095b723d8737f100d41df
yakimka <ss.yakim@gmail.com>
2020-02-28 22:36:35 +0200

    Add github actions workflow (#9)

--- setup.py
+++ setup.py
@@ -9,6 +9,14 @@ setup(
     packages=['CherryTomato'],
     python_requires='>=3.6',
     install_requires=['PyQt5', 'qroundprogressbar'],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-qt',
+            'pytest-mock',
+            'flake8',
+        ]
+    },
     url='https://github.com/yakimka/CherryTomato',
     license='GPL',
     entry_points={'gui_scripts': ['cherry_tomato = CherryTomato.main:main']},
